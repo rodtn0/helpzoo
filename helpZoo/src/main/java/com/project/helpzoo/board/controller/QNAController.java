db668c0e5f1de9e8900279f199caad198cd673c5
dladl <dladl@LAPTOP-D027S9PC>
2020-09-03 17:58:01 +0900

    작성 하는 중

--- helpZoo/src/main/java/com/project/helpzoo/board/controller/QNAController.java
+++ helpZoo/src/main/java/com/project/helpzoo/board/controller/QNAController.java
@@ -7,17 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.helpzoo.board.model.service.QNAService;
 import com.project.helpzoo.board.model.vo.PageInfo;
 import com.project.helpzoo.board.model.vo.QNABoard;
+import com.project.helpzoo.member.model.vo.Member;
 
 @Controller
+@SessionAttributes({"loginMember"})
 @RequestMapping("/qna/*")
 public class QNAController {
 	
@@ -35,9 +39,9 @@ public class QNAController {
 		
 		List<QNABoard> qnaList = qnaService.selectList(pInfo);
 		
-		for(QNABoard b : qnaList) {
-			System.out.println(b);
-		}
+		/*
+		 * for(QNABoard b : qnaList) { System.out.println(b); }
+		 */
 		
 		model.addAttribute("qnaList", qnaList);
 		model.addAttribute("listCount", pInfo.getListCount());
@@ -52,17 +56,21 @@ public class QNAController {
 		return "board/qnaInsert";
 	}
 	
+	// Q&A 글쓰기
 	@RequestMapping(value="insertAction", method=RequestMethod.POST)
-	public String insertAction(QNABoard qnaBoard, Model model,
+	public String insertAction(QNABoard qnaBoard, Model model, String qnaContent,
 			@RequestParam(value="images", required=false) List<MultipartFile> images,
 			HttpServletRequest request,
 			RedirectAttributes rdAttr) {
+		System.out.println(qnaContent);
+		Member loginMember = (Member) model.getAttribute("loginMember");
 		
-		// Member loginMember = (Member) model.getAttribute("loginMember");
+		System.out.println(loginMember);
 		
-		// qnaBoard.setQnaWriter(loginMember.getMemberNo()+"");
-		qnaBoard.setQnaWriter(1+"");
+		qnaBoard.setQnaWriter(loginMember.getMemberNo()+"");
+		
 		System.out.println(qnaBoard);
+		
 		for(int i=0; i<images.size(); i++) {
 			System.out.println("images[" + i + "] :" + images.get(i).getOriginalFilename());
 		}
@@ -72,12 +80,24 @@ public class QNAController {
 		
 		int result = qnaService.insertBoard(qnaBoard, savePath, images);
 		
-		String url =null;
+		String status = null;
+		String msg = null;
+		String url = null;
+		
 		if(result > 0) {
-			
+			status= "success";
+			msg = "등록되었습니다.";
+			url = "qnaList";
+		}else {
+			status= "error";
+			msg= "실패하였습니다.";
+			url = "qnaList";
 		}
 		
-		return null;
+		rdAttr.addFlashAttribute("status", status);
+		rdAttr.addFlashAttribute("msg", msg);
+		
+		return "redirect:" + url;
 		
 	}
 	
