4f6306808ae44a2e675e30fdd21451b5cc3fe602
RainSense <60869441+RainSense@users.noreply.github.com>
2020-09-07 02:05:31 +0900

    시간에 따른 펀딩 프로젝트의 상태 변경, 검색을 위한 클래스 vo 생성,
    pom.xml에 logging과 스케쥴러 설정.

--- helpZoo/src/main/java/com/project/helpzoo/funding/model/dao/FundingDAO.java
+++ helpZoo/src/main/java/com/project/helpzoo/funding/model/dao/FundingDAO.java
@@ -1,34 +1,64 @@
 package com.project.helpzoo.funding.model.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
-import com.project.helpzoo.funding.model.vo.FundingProject;
+import com.project.helpzoo.funding.model.vo.Order;
+import com.project.helpzoo.funding.model.vo.funding.FundingCategory;
+import com.project.helpzoo.funding.model.vo.funding.FundingProject;
+import com.project.helpzoo.funding.model.vo.search.FundingSearch;
+
+
+
 
 @Repository
 public class FundingDAO {
 
 	@PersistenceContext
     EntityManager em ; 
-	 
-	 
-	public List<FundingProject> selectList(int cp) {
 	
-		TypedQuery<FundingProject> query = 
-				em.createQuery("SELECT f FROM FundingProject f ORDER BY f.id",FundingProject.class);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public List<FundingProject> selectList(int cp, FundingSearch fundingSearch) {
+	
+	
+		return null;
+	}
+
+
+	
+	
+	
+	public List<FundingProject> fundingStatusChange() {
 		
-		query.setFirstResult(cp);
-		query.setMaxResults(cp+9);
 		
-		return query.getResultList();
+	return  em.createQuery("select f from  FundingProject f", FundingProject.class)
+        .getResultList();
+		
+		
+		
 	}
 
 	
---
+++ helpZoo/src/main/java/com/project/helpzoo/funding/model/vo/search/SearchSort.java
@@ -0,0 +1,5 @@
+package com.project.helpzoo.funding.model.vo.search;
+
+public enum SearchSort {
+	RECOMMENDATION,LIKE,VIEW,TIME,PARTICIPANTS
+}
