db51dd255fe8b2e48f34a178cada0edb45f1b82b
PC <PC@DESKTOP-CRKT31R>
2020-09-07 12:27:56 +0900

    페이징처리(1)

--- helpZoo/src/main/java/com/project/helpzoo/board/controller/reviewController.java
+++ helpZoo/src/main/java/com/project/helpzoo/board/controller/reviewController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.project.helpzoo.board.model.service.ReviewService;
+import com.project.helpzoo.board.model.vo.PageInfo;
 import com.project.helpzoo.board.model.vo.Review;
 
 @SessionAttributes({"loginMember"})
@@ -22,7 +24,12 @@ public class reviewController {
 	
 	// 펀딩 후기 게시판, 기부 후기 게시판 버튼 누르면 주소에 맞게 목록 조회.
 	@RequestMapping("review/{type}")
-	public String fundingReviewList(@PathVariable int type, Model model) {
+	public String fundingReviewList(@PathVariable int type, Model model,
+								    @RequestParam(value="cp", required=false, defaultValue="1") int cp) {
+												// cp 값이 없어서 1로 받아와짐
+		
+		// 페이징 처리 
+		PageInfo pInfo = reviewService.pagiNation(type, cp);
 		
 		String path = null;
 		List<Review> fReview = null;
@@ -31,14 +38,14 @@ public class reviewController {
 		if(type == 1) {
 			
 			// 펀딩 후기글 리스트 조회
-			fReview = reviewService.selectReviewList(type);
+			fReview = reviewService.selectReviewList(pInfo);
 			System.out.println(fReview);
 			
 			path = "fundingReviewList";
 			
 			
 		}else		  {
-			dReview = reviewService.selectReviewList(type);
+			dReview = reviewService.selectReviewList(pInfo);
 			System.out.println(dReview);
 			
 			path = "donationReviewList";
