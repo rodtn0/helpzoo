142535ff7b5a5dc21f0f89d9710accb162bd746b
user1 <user1@user1-PC>
2020-09-28 10:12:31 +0900

    펀딩 수정

--- helpZoo/src/main/java/com/project/helpzoo/funding/model/service/FundingServiceImpl.java
+++ helpZoo/src/main/java/com/project/helpzoo/funding/model/service/FundingServiceImpl.java
@@ -487,7 +487,7 @@ public class FundingServiceImpl implements FundingService {
 		
 		
 					
-		public String kakaoPayReady(KakaoPayApiItem item, String sisi) {
+		public String kakaoPayReady(KakaoPayApiItem item, String sisi, int orderId) {
 			
 			String HOST = "https://kapi.kakao.com";
 			
@@ -513,7 +513,10 @@ public class FundingServiceImpl implements FundingService {
 	        params.add("tax_free_amount", "0");
 	        
 	        
-	        params.add("approval_url", "http://localhost:8080/helpZoo/fundingAttend/kakaoPaySuccess");
+	        
+	        
+	        //
+	        params.add("approval_url", "http://localhost:8080/helpZoo/fundingAttend/kakaoPaySuccess/" + orderId);
 	        params.add("cancel_url", "http://localhost:8080/helpZoo/fundingAttend/kakaoPayCancel");
 	        params.add("fail_url", "http://localhost:8080/helpZoo/fundingAttend/kakaoPayFail");
 	        
